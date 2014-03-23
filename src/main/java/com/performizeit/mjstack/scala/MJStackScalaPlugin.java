cd5c08bbfcd0b598315f86ebd6031cd3e0c9133a
Haim Yadid <haim@performize-it.com>
2014-03-23 19:24:47 +0200

    plugin api

--- src/main/java/com/performizeit/mjstack/scala/MJStackScalaPlugin.java
+++ src/main/java/com/performizeit/mjstack/scala/MJStackScalaPlugin.java
@@ -1,40 +1,18 @@
 package com.performizeit.mjstack.scala;
 
+import com.performizeit.mjstack.api.Plugin;
+import com.performizeit.mjstack.api.JStackMapper;
+import com.performizeit.mjstack.parser.JStackMetadataStack;
+import com.performizeit.mjstack.parser.JStackStack;
 import com.sparktale.bugtale.server.common.lang.scala.ScalaUtil;
 
-public class MJStackScalaPlugin {
-    static String st="    at com.company.IdentityVerifier$$anonfun$go$2$$anonfun$apply$2.apply$mcII$sp(IdentityVerifier.scala:19)\n" +
-            "    at com.company.IdentityVerifier$$anonfun$go$2$$anonfun$apply$2.apply(IdentityVerifier.scala:17)\n" +
-            "    at com.company.IdentityVerifier$$anonfun$go$2$$anonfun$apply$2.apply(IdentityVerifier.scala:17)\n" +
-            "    at scala.collection.TraversableLike$$anonfun$map$1.apply(TraversableLike.scala:244)\n" +
-            "    at scala.collection.TraversableLike$$anonfun$map$1.apply(TraversableLike.scala:244)\n" +
-            "    at scala.collection.immutable.List.foreach(List.scala:318)\n" +
-            "    at scala.collection.TraversableLike$class.map(TraversableLike.scala:244)\n" +
-            "    at scala.collection.AbstractTraversable.map(Traversable.scala:105)\n" +
-            "    at com.company.IdentityVerifier$$anonfun$go$2.apply(IdentityVerifier.scala:17)\n" +
-            "    at com.company.IdentityVerifier$$anonfun$go$2.apply(IdentityVerifier.scala:16)\n" +
-            "    at scala.collection.TraversableLike$$anonfun$flatMap$1.apply(TraversableLike.scala:251)\n" +
-            "    at scala.collection.TraversableLike$$anonfun$flatMap$1.apply(TraversableLike.scala:251)\n" +
-            "    at scala.collection.immutable.List.foreach(List.scala:318)\n" +
-            "    at scala.collection.TraversableLike$class.flatMap(TraversableLike.scala:251)\n" +
-            "    at scala.collection.AbstractTraversable.flatMap(Traversable.scala:105)\n" +
-            "    at com.company.IdentityVerifier$.go(IdentityVerifier.scala:16)\n" +
-            "    at com.company.UserMap.setLastUserId(UserMap.scala:12)\n" +
-            "    at com.company.UserConsumer.setCurrentUser(UserConsumer.java:69)\n" +
-            "    at com.company.UserConsumer.consume(UserConsumer.java:64)\n" +
-            "    at com.company.UserProducer.execute(UserProducer.java:19)\n" +
-            "    at com.company.UserCreator.execute(UserCreator.java:18)\n" +
-            "    at com.company.UserCreatorMain$1.run(UserCreatorMain.java:37)\n" +
-            "    at com.company.UserCreatorMain.main(UserCreatorMain.java:51)";
-    public static void main(String[] args) {
-        System.out.println(st);
-        String[] lines = st.split("\n");
-        StringBuilder newStack = new StringBuilder();
-        for (String line:lines) {
-            newStack.append(scalafyStackFrame(line)).append("\n");
-        }
-        System.out.println(newStack.toString());
-    }
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Plugin
+public class MJStackScalaPlugin implements JStackMapper{
+
+
 
     private static String scalafyStackFrame(String line) {
         int fnStart = line.indexOf("(") ;
@@ -48,5 +26,30 @@ public class MJStackScalaPlugin {
         String className = pkgClsMthd.substring(pkgClsMthd.lastIndexOf(".")+1);
         String pkg = pkgClsMthd.substring(0,pkgClsMthd.lastIndexOf("."));
         return at+pkg+"."+ ScalaUtil.createScalaFrameTitle(className, method)+fileName;
+    }
+
+    protected static String scalafyStack(String stackTrace) {
+        String[] lines = stackTrace.split("\n");
+        StringBuilder newStack = new StringBuilder();
+        for (String line:lines) {
+            newStack.append(scalafyStackFrame(line)).append("\n");
+        }
+        return newStack.toString();
+    }
+
+    @Override
+    public JStackMetadataStack map(JStackMetadataStack stck) {
+        HashMap<String,Object> mtd = stck.cloneMetaData();
+        JStackStack jss = (JStackStack) mtd.get("stack");
+        jss.setLinesOfStack(scalafyStack(jss.toString()));
+
+
+        return      new JStackMetadataStack(mtd);
+
+    }
+
+    @Override
+    public String getHelpLine() {
+        return "scala plugin - makes the stack trace scala friendly ";
     }
 }
---
+++ src/test/java/com/performizeit/mjstack/scala/MJStackScalaPluginTest.java
@@ -0,0 +1,59 @@
+package com.performizeit.mjstack.scala;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MJStackScalaPluginTest {
+    private static String st="    at com.company.IdentityVerifier$$anonfun$go$2$$anonfun$apply$2.apply$mcII$sp(IdentityVerifier.scala:19)\n" +
+            "    at com.company.IdentityVerifier$$anonfun$go$2$$anonfun$apply$2.apply(IdentityVerifier.scala:17)\n" +
+            "    at com.company.IdentityVerifier$$anonfun$go$2$$anonfun$apply$2.apply(IdentityVerifier.scala:17)\n" +
+            "    at scala.collection.TraversableLike$$anonfun$map$1.apply(TraversableLike.scala:244)\n" +
+            "    at scala.collection.TraversableLike$$anonfun$map$1.apply(TraversableLike.scala:244)\n" +
+            "    at scala.collection.immutable.List.foreach(List.scala:318)\n" +
+            "    at scala.collection.TraversableLike$class.map(TraversableLike.scala:244)\n" +
+            "    at scala.collection.AbstractTraversable.map(Traversable.scala:105)\n" +
+            "    at com.company.IdentityVerifier$$anonfun$go$2.apply(IdentityVerifier.scala:17)\n" +
+            "    at com.company.IdentityVerifier$$anonfun$go$2.apply(IdentityVerifier.scala:16)\n" +
+            "    at scala.collection.TraversableLike$$anonfun$flatMap$1.apply(TraversableLike.scala:251)\n" +
+            "    at scala.collection.TraversableLike$$anonfun$flatMap$1.apply(TraversableLike.scala:251)\n" +
+            "    at scala.collection.immutable.List.foreach(List.scala:318)\n" +
+            "    at scala.collection.TraversableLike$class.flatMap(TraversableLike.scala:251)\n" +
+            "    at scala.collection.AbstractTraversable.flatMap(Traversable.scala:105)\n" +
+            "    at com.company.IdentityVerifier$.go(IdentityVerifier.scala:16)\n" +
+            "    at com.company.UserMap.setLastUserId(UserMap.scala:12)\n" +
+            "    at com.company.UserConsumer.setCurrentUser(UserConsumer.java:69)\n" +
+            "    at com.company.UserConsumer.consume(UserConsumer.java:64)\n" +
+            "    at com.company.UserProducer.execute(UserProducer.java:19)\n" +
+            "    at com.company.UserCreator.execute(UserCreator.java:18)\n" +
+            "    at com.company.UserCreatorMain$1.run(UserCreatorMain.java:37)\n" +
+            "    at com.company.UserCreatorMain.main(UserCreatorMain.java:51)";
+    private static String scalaStack = "    at com.company.IdentityVerifier go λ-2 λ-2 (specialized)(IdentityVerifier.scala:19)\n" +
+            "    at com.company.IdentityVerifier go λ-2 λ-2(IdentityVerifier.scala:17)\n" +
+            "    at com.company.IdentityVerifier go λ-2 λ-2(IdentityVerifier.scala:17)\n" +
+            "    at scala.collection.TraversableLike map λ-1(TraversableLike.scala:244)\n" +
+            "    at scala.collection.TraversableLike map λ-1(TraversableLike.scala:244)\n" +
+            "    at scala.collection.immutable.List foreach(List.scala:318)\n" +
+            "    at scala.collection.TraversableLike map(TraversableLike.scala:244)\n" +
+            "    at scala.collection.AbstractTraversable map(Traversable.scala:105)\n" +
+            "    at com.company.IdentityVerifier go λ-2(IdentityVerifier.scala:17)\n" +
+            "    at com.company.IdentityVerifier go λ-2(IdentityVerifier.scala:16)\n" +
+            "    at scala.collection.TraversableLike flatMap λ-1(TraversableLike.scala:251)\n" +
+            "    at scala.collection.TraversableLike flatMap λ-1(TraversableLike.scala:251)\n" +
+            "    at scala.collection.immutable.List foreach(List.scala:318)\n" +
+            "    at scala.collection.TraversableLike flatMap(TraversableLike.scala:251)\n" +
+            "    at scala.collection.AbstractTraversable flatMap(Traversable.scala:105)\n" +
+            "    at com.company.IdentityVerifier go(IdentityVerifier.scala:16)\n" +
+            "    at com.company.UserMap setLastUserId(UserMap.scala:12)\n" +
+            "    at com.company.UserConsumer setCurrentUser(UserConsumer.java:69)\n" +
+            "    at com.company.UserConsumer consume(UserConsumer.java:64)\n" +
+            "    at com.company.UserProducer execute(UserProducer.java:19)\n" +
+            "    at com.company.UserCreator execute(UserCreator.java:18)\n" +
+            "    at com.company.UserCreatorMain$1 run(UserCreatorMain.java:37)\n" +
+            "    at com.company.UserCreatorMain main(UserCreatorMain.java:51)\n";
+    @Test
+    public void scalafyMyStack() throws Exception {
+        MJStackScalaPlugin pl = new MJStackScalaPlugin();
+       // System.out.println(pl.scalafyStack(st));
+      //  assertEquals(scalaStack,pl.scalafyStack(st));
+    }
+}
