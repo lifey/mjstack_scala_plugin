83ebed46589e04f79fa522cc63c312df4a35c490
Haim Yadid <haim@performize-it.com>
2014-03-31 16:13:48 +0300

    finish

--- src/main/java/com/performizeit/mjstack/scala/MJStackScalaPlugin.java
+++ src/main/java/com/performizeit/mjstack/scala/MJStackScalaPlugin.java
@@ -6,32 +6,30 @@ import com.performizeit.mjstack.parser.JStackMetadataStack;
 import com.performizeit.mjstack.parser.JStackStack;
 import com.sparktale.bugtale.server.common.lang.scala.ScalaUtil;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-@Plugin
-public class MJStackScalaPlugin implements JStackMapper{
-
-
-
+@Plugin(name = "scalafy", paramTypes = {}, description = "scala plugin - makes the stack trace scala friendly")
+public class MJStackScalaPlugin implements JStackMapper {
     private static String scalafyStackFrame(String line) {
-        int fnStart = line.indexOf("(") ;
-        int atStart =  line.indexOf("at") ;
+        if (line.trim().length() ==0) return "";
+        int fnStart = line.indexOf("(");
+        int atStart = line.indexOf("at ");
+        if (fnStart<0 || atStart<0) return line;
         String fileName = line.substring(fnStart);
-        String at = line.substring(0,atStart+3);
-        String pkgClsMthd = line.substring(atStart+3,fnStart);
+        String at = line.substring(0, atStart + 3);
+        String pkgClsMthd = line.substring(atStart + 3, fnStart);
 
-        String method = pkgClsMthd.substring(pkgClsMthd.lastIndexOf(".")+1);
-        pkgClsMthd = pkgClsMthd.substring(0,pkgClsMthd.lastIndexOf("."));
-        String className = pkgClsMthd.substring(pkgClsMthd.lastIndexOf(".")+1);
-        String pkg = pkgClsMthd.substring(0,pkgClsMthd.lastIndexOf("."));
-        return at+pkg+"."+ ScalaUtil.createScalaFrameTitle(className, method)+fileName;
+        String method = pkgClsMthd.substring(pkgClsMthd.lastIndexOf(".") + 1);
+        pkgClsMthd = pkgClsMthd.substring(0, pkgClsMthd.lastIndexOf("."));
+        String className = pkgClsMthd.substring(pkgClsMthd.lastIndexOf(".") + 1);
+        String pkg = pkgClsMthd.substring(0, pkgClsMthd.lastIndexOf("."));
+        return at + pkg + "." + ScalaUtil.createScalaFrameTitle(className, method) + fileName;
     }
 
     protected static String scalafyStack(String stackTrace) {
         String[] lines = stackTrace.split("\n");
         StringBuilder newStack = new StringBuilder();
-        for (String line:lines) {
+        for (String line : lines) {
             newStack.append(scalafyStackFrame(line)).append("\n");
         }
         return newStack.toString();
@@ -39,17 +37,9 @@ public class MJStackScalaPlugin implements JStackMapper{
 
     @Override
     public JStackMetadataStack map(JStackMetadataStack stck) {
-        HashMap<String,Object> mtd = stck.cloneMetaData();
+        HashMap<String, Object> mtd = stck.cloneMetaData();
         JStackStack jss = (JStackStack) mtd.get("stack");
         jss.setLinesOfStack(scalafyStack(jss.toString()));
-
-
-        return      new JStackMetadataStack(mtd);
-
-    }
-
-    @Override
-    public String getHelpLine() {
-        return "scala plugin - makes the stack trace scala friendly ";
+        return new JStackMetadataStack(mtd);
     }
 }
